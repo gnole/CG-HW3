e441f39d9830946c7b914f1da0f0eac4fe7d7f06
gnole <olegbouchin@gmail.com>
2021-10-17 18:11:15 +0300

    draw line

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <SFML/Graphics.hpp>
 #include <unistd.h>
+
 #include <cmath>
+#include <iostream>
 
 void drawLine(int x1, int y1, int x2, int y2, sf::RenderWindow &window) {
 	const int deltaX = abs(x2 - x1);
@@ -26,16 +28,16 @@ void drawLine(int x1, int y1, int x2, int y2, sf::RenderWindow &window) {
 	window.display();
 }
 
-void drawLineWhite(int x1, int y1, int x2, int y2, sf::RenderWindow &window) {
+void drawLineGreen(int x1, int y1, int x2, int y2, sf::RenderWindow &window) {
 	const int deltaX = abs(x2 - x1);
 	const int deltaY = abs(y2 - y1);
 	const int signX = x1 < x2 ? 1 : -1;
 	const int signY = y1 < y2 ? 1 : -1;
 	int error = deltaX - deltaY;
-	sf::Vertex point(sf::Vector2f(x2, y2), sf::Color::White);
+	sf::Vertex point(sf::Vector2f(x2, y2), sf::Color::Green);
 	window.draw(&point, 1, sf::Points);
 	while (x1 != x2 || y1 != y2) {
-		sf::Vertex point1(sf::Vector2f(x1, y1), sf::Color::White);
+		sf::Vertex point1(sf::Vector2f(x1, y1), sf::Color::Green);
 		window.draw(&point1, 1, sf::Points);
 		int error2 = error * 2;
 		if (error2 > -deltaY) {
@@ -83,23 +85,56 @@ void drawCircle(int x0, int y0, int radius, sf::RenderWindow &window) {
 }
 
 int main() {
-	sf::RenderWindow window(sf::VideoMode(500, 500), "HW 2");
-	double alf = 0;
-	const int x0 = 250;
-	const int y0 = 250;
-	const int radius = 99;
+	sf::RenderWindow window(sf::VideoMode(640, 480), "HW3");
+	window.setFramerateLimit(50);
+	std::vector<std::pair<int, int>> vec_points;
+	std::vector<std::pair<int, int>> vec_line;
+	bool dr = false;
 	while (window.isOpen()) {
+
 		sf::Event event;
 		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed)
+			switch (event.type) {
+			case sf::Event::Closed: {
 				window.close();
+				return 0;
+			}
+			case sf::Event::MouseButtonPressed: {
+				if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+					if (dr) {
+						vec_line.push_back(std::make_pair(event.mouseButton.x, event.mouseButton.y));
+					} else {
+						vec_points.push_back(std::make_pair(event.mouseButton.x, event.mouseButton.y));
+					}
+				}
+				if (sf::Mouse::isButtonPressed(sf::Mouse::Right)) {
+					dr = true;
+					auto itend = vec_points.begin();
+					vec_points.push_back(std::make_pair(itend->first, itend->second));
+				}
+			}
+			}
 		}
+
 		window.clear(sf::Color::White);
-		drawCircle(250, 250, 100, window);
-		drawLine(x0, y0, x0 + radius * cos(alf), y0 + radius * sin(alf), window);
-		sleep(1);
-		drawLineWhite(x0, y0, x0 + radius * cos(alf), y0 + radius * sin(alf), window);
-		alf += M_PI / 180 * 6;
+		if (vec_line.size() >= 2) {
+			auto it0 = vec_line.begin();
+			auto it1 = vec_line.begin();
+			++it1;
+			for (; it1 != vec_line.end(); std::advance(it0, 2), std::advance(it1, 2)) {
+				drawLine(it0->first, it0->second, it1->first, it1->second, window);
+			}
+		}
+
+		if (vec_points.size() >= 2) {
+			auto it0 = vec_points.begin();
+			auto it1 = vec_points.begin();
+			++it1;
+			for (; it1 != vec_points.end(); ++it0, ++it1) {
+				drawLineGreen(it0->first, it0->second, it1->first, it1->second, window);
+			}
+		}
+
 		window.display();
 	}
 	return 0;
